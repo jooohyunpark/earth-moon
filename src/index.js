93f70123de05377e0f6b637aac32174ede02cd1f
Joohyun Park <jhp527@nyu.edu>
2020-12-05 22:06:25 -0500

    organize data

--- src/index.js
+++ src/index.js
@@ -5,38 +5,64 @@ const background = svg.querySelector('rect')
 const earth = svg.querySelector('.earth')
 const moon = svg.querySelector('.moon')
 
-const earth_radius = 6371 // km
-const moon_radius = 1737.1 // km
-const distance = 384400 // km
-const target_radius = 1 // cm
-const scale = target_radius / earth_radius
-console.log('scale: ', earth_radius, ':', target_radius)
+// canvas setup
+////////////////////////////////////////////////////////////
+const _width = 84.1
+const _height = 118.9
 
-const canvas_scale = 0.7
-const height = Math.round(1189 * canvas_scale)
-const width = Math.round(841 * canvas_scale)
-console.log('size: ', width, height)
+const canvas_scale = 0.06
+const width = fixed(cm2pix(_width) * canvas_scale, 0)
+const height = fixed(cm2pix(_height) * canvas_scale, 0)
 
-// set canvas size
+console.log('canvas size: ', _width, 'cm', _height, 'cm')
+console.log('---------------------------------')
+console.log('canvas scale: ', canvas_scale) // scale for better development
+console.log('scaled canvas pixel: ', width, 'px', height, 'px')
+console.log('---------------------------------')
+
 svg.setAttribute('width', width)
 svg.setAttribute('height', height)
 background.setAttribute('width', width)
 background.setAttribute('height', height)
+////////////////////////////////////////////////////////////
+
+// data configuration
+////////////////////////////////////////////////////////////
+const _earth_radius = 6371 // km
+const _moon_radius = 1737.1 // km
+const _distance = 384400 // km
+const target_radius = 1 // cm
+const scale = target_radius / _earth_radius
+
+console.log('target radius: ', target_radius, 'cm')
+console.log('scale: ', target_radius, '/', _earth_radius)
+console.log('---------------------------------')
+
+console.log('earth radius: ', fixed(_earth_radius * scale), 'cm')
+console.log('moon radius: ', fixed(_moon_radius * scale), 'cm')
+console.log('distance: ', fixed(_distance * scale), 'cm')
+console.log('---------------------------------')
 
 // earth
-const earth_pos = {
-  x: 0,
-  y: 0
-}
+const earth_radius = cm2pix(_earth_radius * scale) * canvas_scale
+console.log('scaled earth radius pixel: ', earth_radius, 'px')
 
-// draw earth
+// moon
+const moon_radius = cm2pix(_moon_radius * scale) * canvas_scale
+console.log('scaled moon radius pixel: ', moon_radius, 'px')
+// draw
+////////////////////////////////////////////////////////////
+// earth
 earth.setAttribute('x', 100)
 earth.setAttribute('y', 100)
-earth.setAttribute('width', 100)
-earth.setAttribute('height', 100)
-earth.setAttribute('transform', `translate(${-50},${-50})`)
+earth.setAttribute('width', earth_radius * 2)
+earth.setAttribute('height', earth_radius * 2)
+earth.setAttribute(
+  'transform',
+  `translate(${-1 * earth_radius},${-1 * earth_radius})`
+)
 
-// draw moon
+// moon
 moon.setAttribute('x', 100)
 moon.setAttribute('y', 100)
 moon.setAttribute('width', 100)
@@ -60,3 +86,13 @@ function svgDataURL(svg) {
 document.addEventListener('keydown', e => {
   if (e.which === 32) download()
 })
+
+function cm2pix(value) {
+  const dpi = 300
+  const ratio = 2.54 / dpi
+  return fixed(value / ratio)
+}
+
+function fixed(val, n = 2) {
+  return Number(parseFloat(val).toFixed(n))
+}
